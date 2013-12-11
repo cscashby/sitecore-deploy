02b8565f4b8ad205be410118f751f6cae1cfd423
Christian Ashby <git@cashby.me.uk>
2013-12-11 08:11:37 +0000

    First checkin with beginnings of drive config scripts

---
+++ scripts/prereqs.ps1
@@ -0,0 +1,24 @@
+Install-WindowsFeature -ConfigurationFilePath .\DeploymentConfigTemplate.xml -Restart
+
+Get-Disk |
+	Where-Object -FilterScript {
+		$_.IsOffline -Eq $true
+	} |
+		ForEach-Object {
+			$diskNumber = $_.Number
+			write-host "Setting disk $($diskNumber) as not readonly" -ForegroundColor "Green"
+			Set-Disk -Number $diskNumber -isReadOnly $false
+			try {
+				Get-Partition -DiskNumber $diskNumber |
+					ForEach-Object {
+						write-host "Removing partition $($_.PartitionNumber)" -ForegroundColor "Green"
+						Remove-Partition -PartitionNumber $_.PartitionNumber -DiskNumber $diskNumber -Confirm:$false
+					}
+			} catch {
+				write-host "Caught an exception:" -ForegroundColor "Red"
+				write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor "Red"
+				write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor "Red"
+			}
+			write-host -ForegroundColor "Green" "Creating D" 
+			New-Partition -DiskNumber $diskNumber -UseMaximumSize -AssignDriveLetter -DriveLetter "D"
+		}
